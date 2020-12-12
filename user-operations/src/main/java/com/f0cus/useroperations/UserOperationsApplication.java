d84ae3ee3d472f0523ba0bfade378f96aaaafca4
Aman Thapliyal <aman.thapliyal791@gmail.com>
2020-12-12 22:31:24 +0530

    project-dawn/user-operations project started

---
+++ user-operations/src/main/java/com/f0cus/useroperations/UserOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.f0cus.useroperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserOperationsApplication.class, args);
+	}
+
+}
